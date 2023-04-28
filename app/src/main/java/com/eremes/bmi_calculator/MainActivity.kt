b35514f77294d09eef87d0e3cd0ea315b3c660ce
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-04-28 18:17:54 +0900

    HungryDeveloper

---
+++ app/src/main/java/com/eremes/bmi_calculator/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.eremes.bmi_calculator
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
+
