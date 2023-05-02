f369711aeb448e10ed0f4bba8713c8ff27311900
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-05-02 11:01:53 +0900

    fix: main activity replace to scrollview instead linearlayout

--- app/src/main/java/com/eremes/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/eremes/bmi_calculator/MainActivity.kt
@@ -9,12 +9,22 @@ import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cardViewStarters: CardView
+    lateinit var cardViewMains: CardView
+
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cardViewStarters = findViewById(R.id.cardViewStarters)
+        cardViewMains = findViewById(R.id.cardViewMains)
+
     }
 
 }
